f0a515d0992a56d02f5a68df820f6f5f09389ae3
Rodrigo Rodrigues <rodrigo.rodrigues.live@hotmail.com>
2022-05-31 08:52:03 -0300

    Commit

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,21 @@
+/**
+ * name     => string
+ * duration => number
+ * educator => string
+ */
+
+interface Couse{
+    name: string;
+    duration?: number;
+    educator: string
+}
+
+class CreateCourseService{    
+    
+    execute({duration = 8, educator, name}: Couse){
+        console.log(name, duration, educator);
+    }
+
+}
+
+export default new CreateCourseService();
